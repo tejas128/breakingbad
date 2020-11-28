4ef6fc1232f662ca97f32b3025239b100cdd31b5
tejas128 <ladtejas128@gmail.com>
2020-11-28 13:46:15 +0530

    Updates

---
+++ precache-manifest.e4c8213f01464c2f11e216284eb1f922.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18f731c2ea4692a5715867b434fa31fb",
+    "url": "/breakingbad/index.html"
+  },
+  {
+    "revision": "9dac649413a525a49d01",
+    "url": "/breakingbad/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c534606484eca602df68",
+    "url": "/breakingbad/static/css/main.1f11ef54.chunk.css"
+  },
+  {
+    "revision": "9dac649413a525a49d01",
+    "url": "/breakingbad/static/js/2.5aacdb64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breakingbad/static/js/2.5aacdb64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c534606484eca602df68",
+    "url": "/breakingbad/static/js/main.800ea18e.chunk.js"
+  },
+  {
+    "revision": "217ad1789a2d922379f6",
+    "url": "/breakingbad/static/js/runtime-main.20b04436.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breakingbad/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breakingbad/static/media/logo.3795f66d.png"
+  }
+]);
